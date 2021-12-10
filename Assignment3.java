464e59eef9898dad9dbcbf4513f06d8cf0e6f014
sdsmk <34293493+sdsmk@users.noreply.github.com>
2021-12-10 18:17:07 +0530

    Update and rename ThrowschildEg.java to Assignment3.java

--- Assignment3.java
+++ Assignment3.java
@@ -6,14 +6,14 @@ class Base{
 		System.out.println("Base method");
 	}
 }
-class ThrowschildEg extends Base{
+class Assignment3 extends Base{
 	
 		void method() throws ArithmeticException{
 				System.out.println("Dervied Class overridden the Base with Unchecked");
 			}
 	
 	public static void main(String args[]) {
-		Base b = new ThrowschildEg();
+		Base b = new Assignment3();
 	    b.method();
 	}
 }
